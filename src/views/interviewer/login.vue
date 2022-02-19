288f2fa56953194f5318a4fba9e45f476800a134
lzc <1064827638@qq.com>
2022-02-19 22:44:56 +0800

    获取code

--- src/views/interviewer/login.vue
+++ src/views/interviewer/login.vue
@@ -1,8 +1,11 @@
 <template>
     login
+    <div>{{code}}</div>
 </template>
 <script setup lang="ts">
+import { onMounted, ref } from 'vue'
 
+const code = ref()
 function getQueryString(name:string) {
   const reg = new RegExp(`(^|&)${name}=([^&]*)(&|$)`, 'i');
   const result = window.location.search.substring(1).match(reg);
@@ -11,9 +14,9 @@ function getQueryString(name:string) {
   }
   return null;
 }
-
-// 通过原生 JS 去获取 code
-console.log(getQueryString('code'))
+onMounted(() => {
+    code.value = getQueryString('code')
+})
 
 </script>
 <style lang="less" scoped></style>
