3d534d4f96bfeb52f7eb363ce32970bce26f186a
lzc <1064827638@qq.com>
2022-02-19 22:40:47 +0800

    获取code

--- src/views/interviewer/login.vue
+++ src/views/interviewer/login.vue
@@ -1,11 +1,19 @@
 <template>
     login
-    <div>{{Route.query.code}}</div>    
 </template>
 <script setup lang="ts">
-import { useRoute } from 'vue-router'
 
-const Route = useRoute()
+function getQueryString(name:string) {
+  const reg = new RegExp(`(^|&)${name}=([^&]*)(&|$)`, 'i');
+  const result = window.location.search.substring(1).match(reg);
+  if (result != null) {
+    return decodeURIComponent(result[2]);
+  }
+  return null;
+}
+
+// 通过原生 JS 去获取 code
+console.log(getQueryString('code'))
 
 </script>
 <style lang="less" scoped></style>
