bbd65274920e21c060f6cb25d9a98a9d4cf2f9d3
lzc <1064827638@qq.com>
2022-02-28 22:23:31 +0800

    push

--- src/commons/request/interviewer.ts
+++ src/commons/request/interviewer.ts
@@ -49,7 +49,7 @@ request.interceptors.response.use((response) => {
     }
 
     if (response.data.code !== 1){
-        if (response.data.code === 2){
+        if (response.data.code === 2 || response.data.code === 4){
             ElMessage.error(response.data.code)
             window.location.href = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=wwe0d87bd068758439&redirect_uri=${encodeURIComponent('http://weather-report.xdwizz.top/#/login')}&response_type=code&scope=snsapi_base&state=${route.path}#wechat_redirect`
         }
