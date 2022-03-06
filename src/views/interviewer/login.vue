6069ea25ba5b90b3f2cab2d63ea20217349101cc
lzc <1064827638@qq.com>
2022-03-06 20:40:06 +0800

    push

--- src/views/interviewer/login.vue
+++ src/views/interviewer/login.vue
@@ -15,7 +15,7 @@ function getQueryString(name:string) {
   const reg = new RegExp(`(^|&)${name}=([^&]*)(&|$)`, 'i');
   const result = window.location.search.substring(1).match(reg);
   if (result != null) {
-    return decodeURIComponent(result[2]);
+    return decodeURIComponent(result[2]); 
   }
   return null;
 }
