908cfa33f640ea5dcde7bfa2ed11c4d70511cf95
faga1 <1064827638@qq.com>
2022-03-29 22:34:40 +0800

    push

--- src/commons/request/interviewer.ts
+++ src/commons/request/interviewer.ts
@@ -26,7 +26,7 @@ const codeMessage:message = {
 	502: "网关错误。",
 	503: "服务不可用，服务器暂时过载或维护。",
 	504: "网关超时。",
-}; 
+};
 const request = axios.create({
     baseURL: 'https://orientation-system.be.wizzstudio.com',
 })
